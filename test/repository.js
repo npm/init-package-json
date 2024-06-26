fa827d2d8bdc0b1caca109f087f322e4db5c4c26
Marshall Ku <marshall@kakao.com>
2024-06-27 08:27:07 +0900

    chore: Add test case for retrieving repository URL

--- test/repository.js
+++ test/repository.js
@@ -1,3 +1,4 @@
+const { exec } = require('node:child_process')
 const t = require('tap')
 const { setup, child, isChild } = require('./fixtures/setup')
 
@@ -33,5 +34,51 @@ t.test('license', async (t) => {
     },
     main: 'index.js',
   }
+  t.has(data, wanted)
+})
+
+const getRemoteURL = () => new Promise((res, rej) => {
+  exec('git config --get remote.origin.url', (err, stdout) => {
+    if (err) {
+      rej(err)
+    } else {
+      res(stdout.trim())
+    }
+  })
+})
+
+t.test('parse repository from git config', async (t) => {
+  const { data } = await setup(t, __filename, {
+    inputs: [
+      'the-name', // package name
+      '', // version
+      '', // description
+      '', // entry point
+      '', // test
+      '', // git repo
+      '', // keywords
+      '', // author
+      '', // license
+      'yes', // about to write
+    ],
+  })
+  const remoteURL = await getRemoteURL()
+
+  const wanted = {
+    name: 'the-name',
+    version: '1.0.0',
+    description: '',
+    scripts: { test: 'echo "Error: no test specified" && exit 1' },
+    author: '',
+    main: 'index.js',
+  }
+
+  if (remoteURL) {
+    wanted.repository = {
+      type: 'git',
+      url: `git+${remoteURL}`,
+    }
+  }
+
   t.has(data, wanted)
 })
