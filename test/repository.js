c3d7b0c615eb7c30eeb6b540423423c16b31df1b
Marshall Ku <marshall@kakao.com>
2024-06-28 09:49:58 +0900

    chore: Fix to use temporary directory in test environment

--- test/repository.js
+++ test/repository.js
@@ -1,10 +1,39 @@
-const { exec } = require('node:child_process')
+const path = require('node:path')
+const fs = require('fs/promises')
 const t = require('tap')
 const { setup, child, isChild } = require('./fixtures/setup')
 
 if (isChild()) {
   return child()
 }
+
+const cwd = process.cwd()
+let updateContent
+
+t.beforeEach(async () => {
+  const testdir = t.testdir({
+    '.git': {
+      config: '',
+    },
+  })
+
+  updateContent = (content) => {
+    const gitConfigPath = path.resolve(testdir, '.git/config')
+    return fs.writeFile(gitConfigPath, content)
+  }
+
+  // Copy package.json
+  await fs.copyFile(
+    path.resolve(cwd, 'package.json'),
+    path.resolve(testdir, 'package.json')
+  )
+
+  process.chdir(testdir)
+})
+
+t.afterEach(() => {
+  process.chdir(cwd)
+})
 
 t.test('license', async (t) => {
   const { data } = await setup(t, __filename, {
@@ -37,17 +66,21 @@ t.test('license', async (t) => {
   t.has(data, wanted)
 })
 
-const getRemoteURL = () => new Promise((res, rej) => {
-  exec('git config --get remote.origin.url', (err, stdout) => {
-    if (err) {
-      rej(err)
-    } else {
-      res(stdout.trim())
-    }
-  })
-})
-
 t.test('parse repository from git config', async (t) => {
+  await updateContent(`
+[core]
+    repositoryformatversion = 0
+    filemode = true
+    bare = false
+    logallrefupdates = true
+[remote "origin"]
+    url = https://github.com/npm/init-package-json.git
+    fetch = +refs/heads/*:refs/remotes/origin/*
+[branch "main"]
+    remote = origin
+    merge = refs/heads/main
+`)
+
   const { data } = await setup(t, __filename, {
     inputs: [
       'the-name', // package name
@@ -62,7 +95,6 @@ t.test('parse repository from git config', async (t) => {
       'yes', // about to write
     ],
   })
-  const remoteURL = await getRemoteURL()
 
   const wanted = {
     name: 'the-name',
@@ -70,14 +102,11 @@ t.test('parse repository from git config', async (t) => {
     description: '',
     scripts: { test: 'echo "Error: no test specified" && exit 1' },
     author: '',
-    main: 'index.js',
-  }
-
-  if (remoteURL) {
-    wanted.repository = {
+    repository: {
       type: 'git',
-      url: `git+${remoteURL}`,
-    }
+      url: `git+https://github.com/npm/init-package-json.git`,
+    },
+    main: 'index.js',
   }
 
   t.has(data, wanted)
