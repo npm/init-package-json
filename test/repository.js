21fc172e9ed4be3666c7a8241f1cd51686a8c36a
Milan Meva <milaninfy@github.com>
2024-12-02 08:34:19 -0500

    fix: default git repository value if exist

--- test/repository.js
+++ test/repository.js
@@ -35,3 +35,23 @@ t.test('license', async (t) => {
   }
   t.has(data, wanted)
 })
+
+t.test('repository from git config', async (t) => {
+  const testdir = {
+    '.git': {
+      config: `
+[remote "origin"]
+  url = https://github.com/npm/cli.git`,
+    } }
+
+  const { data } = await setup(t, __filename, {
+    config: { yes: 'yes' },
+    testdir,
+  })
+
+  const wanted = {
+    type: 'git',
+    url: 'git+https://github.com/npm/cli.git',
+  }
+  t.has(data.repository, wanted)
+})
