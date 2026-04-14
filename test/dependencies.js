cddad4fe4ddc5b712793d078c6d3dac1d8427ed1
Gar <gar+gh@danger.computer>
2026-04-14 11:50:28 -0700

    fix!: stop auto populating dependencies (#349)
    BREAKING CHANGE: existing packages in `node_modules` are no longer used
    to try to pre-populate dependencies and devDependencies
    
    fixes https://github.com/npm/statusboard/issues/1075

--- test/dependencies.js
+++ test/dependencies.js
@@ -5,7 +5,7 @@ if (isChild()) {
   return child({ chdir: true })
 }
 
-t.test('read in dependencies and dev deps', async (t) => {
+t.test('existing dependencies', async (t) => {
   const testdirContents = {
     'package.json': JSON.stringify({
       dependencies: {
@@ -16,16 +16,6 @@ t.test('read in dependencies and dev deps', async (t) => {
         abbrev: '*',
       },
     }),
-    node_modules: {},
-  }
-
-  for (const fakedep of ['mocha', 'tap', 'async', 'foobar']) {
-    testdirContents.node_modules[fakedep] = {
-      'package.json': JSON.stringify({
-        name: fakedep,
-        version: '1.0.0',
-      }),
-    }
   }
 
   const { data } = await setup(t, __filename, {
@@ -34,22 +24,34 @@ t.test('read in dependencies and dev deps', async (t) => {
   })
 
   t.same(data, {
-    name: 'tap-testdir-dependencies-read-in-dependencies-and-dev-deps',
+    name: 'tap-testdir-dependencies-existing-dependencies',
     version: '1.0.0',
     type: 'commonjs',
     description: '',
     author: '',
-    scripts: { test: 'mocha' },
+    scripts: { test: 'echo "Error: no test specified" && exit 1' },
     main: 'index.js',
     keywords: [],
     dependencies: {
       tap: '*',
     },
-    devDependencies: {
-      mocha: '^1.0.0',
-    },
     optionalDependencies: {
       abbrev: '*',
     },
   }, 'used the correct dependency information')
+})
+
+t.test('delete empty dependencies', async (t) => {
+  const testdirContents = {
+    'package.json': JSON.stringify({
+      dependencies: {},
+    }),
+  }
+
+  const { data } = await setup(t, __filename, {
+    testdir: testdirContents,
+    config: { yes: 'yes', 'save-prefix': '^' },
+  })
+
+  t.same(data.dependencies, undefined, 'empty dependencies is removed')
 })
